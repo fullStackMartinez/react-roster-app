548615c1506ad3ee6001f061a3a338028066ef69
Esteban Martinez <fullstackmartinez@gmail.com>
2019-01-03 23:58:46 -0700

    added form to Roster.js component, added event listeners to input field and form

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class Roster extends Component {
+	constructor() {
+		super();
+		this.state = {
+			title: 'Player Roster',
+			newPlayer: ''
+		}
+	}
+//Define function that will fire when we click our button
+	formSubmitted(event) {
+		event.preventDefault();
+		console.log(this.state.newPlayer);
+	}
+
+	//Define listener to our input as a function
+	newPlayerAdded(event) {
+		this.setState({
+			newPlayer: event.target.value
+		})
+	}
 	render() {
 		return (
 			<div>
-				<h1>Player Roster</h1>
+				<h1>{this.state.title}</h1>
+
+				<form onSubmit={this.formSubmitted.bind(this)}>
+
+					<label htmlFor="newRoster">Add Player to Roster</label>
+
+					<input onChange={this.newPlayerAdded.bind(this)} id="newRoster" name="newRoster" />
+
+					<button type="submit">Add Player</button>
+
+				</form>
 			</div>
 		);
 	}
